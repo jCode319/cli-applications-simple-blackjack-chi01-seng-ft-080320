57550f00a1547c2678b23c5ad5e1247454dc81b0
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 05:55:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,7 +23,7 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(total)
+def initial_round
   total = deal_card + deal_card
   total
 end
