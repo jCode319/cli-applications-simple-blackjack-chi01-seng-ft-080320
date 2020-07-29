e133f5a06c69f62f6c870fc86e20b579d9109ef9
Jarrell Jackson <jdjack3@gmail.com>
2020-07-29 06:11:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,7 +30,8 @@ def initial_round
 end
 
 def hit?(get_user_input)
-  calls on "#{prompt_user}" and receives "#{get_user_input}"
+  "#{prompt_user}"
+  "#{get_user_input}"
 end
 
 def invalid_command
